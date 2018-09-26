5f035bccd2ba4f498c49e3b57c4c47534b9bed28
may4bdul <43436357+may4bdul@users.noreply.github.com>
2018-09-26 17:04:10 +0800

    Add files via upload

---
+++ BreathingSquare.py
@@ -0,0 +1,60 @@
+import math, numpy, random #handy system and math functions
+from psychopy import core, event, visual, gui #these are the psychopy modules
+
+myWin = visual.Window(color='white', units='pix', size=[1000,1000], allowGUI=False, fullscr=False)#creates a window 
+myClock = core.Clock() #this creates and starts a clock which we can later read
+
+movingSquare = visual.Rect(myWin, width=200, height=200, fillColor='red', lineColor=None)
+occludingSquare = visual.Rect(myWin, width=200, height=200, fillColor='black', lineColor=None)
+
+myScale = visual.RatingScale(myWin, pos=[0, -360], low=100, high=180,  textSize=0.5, lineColor='black',  tickHeight=False, scale=None, showAccept=False, singleClick=True)
+information=visual.TextStim(myWin, pos=[0,-385], text='', height=18, color='black') 
+title=visual.TextStim(myWin, pos=[0,305], text='Breathing Square Illusion', height=24, color='green') 
+
+# draw four occluders at a distance 'distance'
+def drawOccluders(distance= 120): 
+
+    for xposition in [distance,-distance]:
+        for yposition in [distance,-distance]:
+            occludingSquare.setPos([xposition,yposition])
+            occludingSquare.draw()
+
+# the main loop
+def mainLoop(distanceOfOccluders =120):
+    
+    finished = False
+
+    while not finished:
+        
+        movingSquare.draw()
+        movingSquare.setOri(2, operation="+")
+        drawOccluders(distanceOfOccluders)
+        
+        title.draw()
+        information.draw()
+        myScale.draw()
+        myWin.flip()
+
+        if movingSquare.ori >= 360: 
+            movingSquare.ori =0
+        
+        if myScale.noResponse ==False: #some new value has been selected with the mouse
+            distanceOfOccluders = myScale.getRating()
+            information.setText(str(distanceOfOccluders))
+            myScale.reset()
+    
+        if event.getKeys(keyList=['escape']): 
+            finished =True
+    
+        waitUntil = myClock.getTime() + 1 / 60.   # one second divided by 60 (most monitors are 60Hz), which is 0.016..
+        while myClock.getTime() <waitUntil:
+            pass
+    
+mainLoop() #enters the main loop
+myWin.close() #closes the window
+core.quit() #quits
+
+
+
+
+
